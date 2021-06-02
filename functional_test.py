488a2e4047ea56391822fd743243ae18ca0e59b0
hagamenoncortez <hagamenoncortez@hotmail.com>
2021-06-01 23:14:28 -0300

    Improve history of functional test

--- functional_test.py
+++ functional_test.py
@@ -1,6 +1,55 @@
+############################## INICIO ####################################
+
 from selenium import webdriver
 
 browser = webdriver.Chrome()
+
+
+# Edith ouviu falar que agora a aplicação online de lista de tarefas
+# aceita definir prioridades nas tarefas do tipo baixa, média e alta
+# Ela decide verificar a homepage
+  
 browser.get("http://localhost:8000")
 
-assert 'worked successfully' in browser.title
+
+# Ela percebe que o título da página e o cabeçalho mencionam
+# listas de tarefas com prioridade (priority to-do)
+      
+assert 'Priority To-Do' in browser.title
+        
+# Ela é convidada a inserir um item de tarefa e a prioridade da
+# mesma imediatamente
+
+
+# Ela digita "Comprar anzol" em uma nova caixa de texto
+# e assinala prioridade alta no campo de seleção de prioridades
+
+
+# Quando ela tecla enter, a página é atualizada, e agora
+# a página lista "1 - Comprar anzol - prioridade alta"
+# como um item em uma lista de tarefas
+
+
+# Ainda continua havendo uma caixa de texto convidando-a a
+# acrescentar outro item. Ela insere "Comprar cola instantâne"
+
+# e assinala prioridade baixa pois ela ainda tem cola suficiente
+# por algum tempo
+
+
+# A página é atualizada novamente e agora mostra os dois
+# itens em sua lista e as respectivas prioridades
+
+
+# Edith se pergunta se o site lembrará de sua lista. Então
+# ela nota que o site gerou um URL único para ela -- há um
+# pequeno texto explicativo para isso.
+
+
+# Ela acessa essa URL -- sua lista de tarefas continua lá.
+
+browser.quit()
+
+
+################################# FIM ####################################
+
